20fb82397bc4ae6fa3e63b9e1e5ee9de20c8123c
deca <git@deca.gg>
2023-08-04 14:48:55 -0700

    💚 Experimental net7 builder for net6

--- ECSExtensions.cs
+++ ECSExtensions.cs
@@ -1,4 +1,5 @@
 
+using System;
 using Bloodstone.API;
 using Il2CppInterop.Runtime;
 using Unity.Entities;
@@ -6,7 +7,9 @@ using Unity.Entities;
 
 /// <remarks>
 /// Copying these around is an anti-pattern - they should live in Bloodstone. I've put them here for now to uncouple things.
+/// CS8500
 /// </remarks>
+#pragma warning disable CS8500
 internal static class ECSExtensions
 {
 	internal static void With<T>(this Entity entity, VExtensions.ActionRef<T> action) where T : struct
@@ -16,24 +19,33 @@ internal static class ECSExtensions
 		VWorld.Game.EntityManager.SetComponentData(entity, item);
 	}
 
-	internal static bool Has<T>(this Entity entity)
+	internal static bool Has<T>(this Entity entity) where T : struct
 	{
 		int typeIndex = TypeManager.GetTypeIndex(Il2CppType.Of<T>());
 		
 		return VWorld.Game.EntityManager.HasComponentRaw(entity, typeIndex);
 	}
 
-	internal unsafe static T RW<T>(this Entity entity)
+	internal unsafe static T RW<T>(this Entity entity) where T: struct
 	{
 		int typeIndex = TypeManager.GetTypeIndex(Il2CppType.Of<T>());
 		T* componentDataRawRW = (T*)VWorld.Game.EntityManager.GetComponentDataRawRW(entity, typeIndex);
+		if (componentDataRawRW == null)
+		{
+			throw new InvalidOperationException($"Failure to access ReadWrite <{typeof(T).Name}> typeIndex({typeIndex}) on entity({entity}).");
+		}
 		return *componentDataRawRW;
 	}
 
-	internal unsafe static T Read<T>(this Entity entity)
+	internal unsafe static T Read<T>(this Entity entity) where T : struct
 	{
 		int typeIndex = TypeManager.GetTypeIndex(Il2CppType.Of<T>());
 		T* componentDataRawRO = (T*)VWorld.Game.EntityManager.GetComponentDataRawRO(entity, typeIndex);
+		if(componentDataRawRO == null)
+		{
+			throw new InvalidOperationException($"Failure to access ReadOnly <{typeof(T).Name}> typeIndex({typeIndex}) on entity({entity}).");
+		}
 		return *componentDataRawRO;
 	}
 }
+#pragma warning restore CS8500
